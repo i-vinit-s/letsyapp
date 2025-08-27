4dd82cfdc173ef18844abe3838b1a27d5d8eaad6
Vinit / <vinitsiwach06@gmail.com>
2025-08-28 00:52:19 +0530

    fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -97,7 +97,7 @@ export default function Navbar() {
                 </Link>
               ) : (
                 <Link
-                  href="/c/new"
+                  href="/c/create"
                   className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 font-medium"
                 >
                   Create Page
@@ -168,7 +168,7 @@ export default function Navbar() {
                     </Link>
                   ) : (
                     <Link
-                      href="/c/new"
+                      href="/c/create"
                       className="block w-full text-center px-4 py-2 bg-blue-600 rounded-lg hover:bg-blue-700 text-white font-medium"
                       onClick={() => setIsOpen(false)}
                     >
